764712312b70828e4156a2fcce165164fefd8c8b
Vterebenin <niceserver11@mail.ru>
2018-05-28 21:47:06 +0300

    add main_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
 	def markdown(source)
     Kramdown::Document.new(source).to_html.html_safe
   end
+
+  def main_title
+  	if params[:controller] == "posts"
+  		"My words"
+  	elsif params[:controller] == "projects"
+  		"My addiction"
+  	elsif params[:controller] == "static_pages" && params[:action] == "about"
+  		"Myself"
+  	elsif params[:controller] == "static_page" && params[:action] == "contact"
+  		"My connections"
+  	else
+  		""
+  	end
+  end
 end
